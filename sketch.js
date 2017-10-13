318bc89f3e375bb5bc47d88f4776587d31ca189a
lkang1 <31940527+lkang1@users.noreply.github.com>
2017-10-13 15:30:05 -0500

    new files

---
+++ sketch.js
@@ -0,0 +1,58 @@
+function setup() {
+  createCanvas(400,300);
+
+}
+
+function draw() {
+  background(150);
+  noStroke();
+  // circle1
+  fill(66, 226, 244);
+  ellipse(160,160,120,120);
+  // milliscricle1
+  fill(81, 166, 232);
+  ellipse(160,160,millis()%120,millis()%120);
+  // circle2
+  fill(247, 186, 89);
+  ellipse(100,120,40,40);
+  // milliscricle2
+  fill(239, 226, 47);
+  ellipse(100,120,millis()%40,millis()%40);
+  // circle3
+  fill(147, 242, 128);
+  ellipse(220,100,80,80);
+  // milliscricle3
+  fill(207, 247, 150);
+  ellipse(220,100,millis()%80,millis()%80);
+  // circle4
+  fill(229, 50, 196);
+  ellipse(250,180,50,50);
+  // milliscricle4
+  fill(249, 189, 238);
+  ellipse(250,180,millis()%50,millis()%50);
+  // circle5
+  fill(66, 226, 244);
+  ellipse(320,120,120,120);
+  // milliscricle5
+  fill(81, 166, 232);
+  ellipse(320,120,millis()%120,millis()%120);
+  // circle6
+  fill(247, 186, 89);
+  ellipse(200,240,40,40);
+  // milliscricle6
+  fill(239, 226, 47);
+  ellipse(200,240,millis()%40,millis()%40);
+  // rect1
+  // fill(81, 166, 232);
+  // rec(200,100,100,100);
+
+
+
+
+
+
+
+
+
+
+}
